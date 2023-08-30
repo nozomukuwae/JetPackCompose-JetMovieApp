38295029536a813522a7a3f29edd367b4ad45108
Nozomu Kuwae <nozomukuwae@gmail.com>
2023-08-30 19:47:50 +0900

    Add back button inside screen

--- app/src/main/java/com/example/jetmovieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/jetmovieapp/screens/details/DetailsScreen.kt
@@ -2,23 +2,46 @@ package com.example.jetmovieapp.screens.details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun DetailsScreen(navController: NavController, title: String?) {
+    DetailsScreenContent(navController = navController, title = title)
+}
+
+@Composable
+fun DetailsScreenContent(navController: NavController = rememberNavController(), title: String?) {
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             Text(text = title.toString(), style = MaterialTheme.typography.h5)
+            Spacer(modifier = Modifier.height(23.dp))
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go back")
+            }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailsScreenPreview() {
+    DetailsScreenContent(title = "Avatar")
 }
