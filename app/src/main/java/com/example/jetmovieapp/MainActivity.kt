50235d9417cad6e8962c6c5f0c1241ec7c1291c3
Nozomu Kuwae <nozomukuwae@gmail.com>
2023-08-30 18:11:11 +0900

    Embed in Scaffold

--- app/src/main/java/com/example/jetmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/jetmovieapp/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetmovieapp.ui.theme.JetMovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +28,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     JetMovieAppTheme {
-        content()
+        Scaffold(topBar = {
+            TopAppBar(
+                backgroundColor = Color.Magenta,
+                elevation = 5.dp
+            ) {
+                Text(text = "Movies")
+            }
+        }) {
+            content()
+        }
     }
 }
 
