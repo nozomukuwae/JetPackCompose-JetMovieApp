81c67313960f7ee09bab88b909a1c6b55e870918
Nozomu Kuwae <nozomukuwae@gmail.com>
2023-08-30 19:58:37 +0900

    Embed details screen in scaffold
    Move back button to top bar

--- app/src/main/java/com/example/jetmovieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/jetmovieapp/screens/details/DetailsScreen.kt
@@ -1,17 +1,24 @@
 package com.example.jetmovieapp.screens.details
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -24,17 +31,33 @@ fun DetailsScreen(navController: NavController, title: String?) {
 
 @Composable
 fun DetailsScreenContent(navController: NavController = rememberNavController(), title: String?) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    Scaffold(topBar = {
+        TopAppBar(
+            backgroundColor = Color.Magenta,
+            elevation = 5.dp
         ) {
-            Text(text = title.toString(), style = MaterialTheme.typography.h5)
-            Spacer(modifier = Modifier.height(23.dp))
-            Button(onClick = {
-                navController.popBackStack()
-            }) {
-                Text(text = "Go back")
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "back",
+                    modifier = Modifier.clickable {
+                        navController.popBackStack()
+                    }
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = "Details")
+            }
+        }
+    }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = title.toString(), style = MaterialTheme.typography.h5)
             }
         }
     }
